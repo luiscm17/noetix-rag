bc237178fe74186343b251364d1d552f92652986
luis-cm-linux <115508520+luiscm17@users.noreply.github.com>
2026-03-10 23:04:37 -0400

    feat: update domain interfaces init with new authentication interfaces

--- src/domain/interfaces/__init__.py
+++ src/domain/interfaces/__init__.py
@@ -1,1 +1,11 @@
-# PDF Reader with AI - Main package
+from src.domain.interfaces.user_repository import IUserRepository
+from src.domain.interfaces.document_repository import IDocumentRepository
+from src.domain.interfaces.password_hasher import IPasswordHasher
+from src.domain.interfaces.token_generator import ITokenGenerator
+
+__all__ = [
+    "IUserRepository",
+    "IDocumentRepository",
+    "IPasswordHasher",
+    "ITokenGenerator",
+]
